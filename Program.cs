ed8636715fe7b7004d409bc4928430aecb7ac0d6
jupppiter <jupiter.asterios@gmail.com>
2015-09-05 22:35:55 +0300

    1

--- Program.cs
+++ Program.cs
@@ -49,6 +49,7 @@ namespace CTDominion
 //	    if (Player.IsDead && Items.CanUseItem(Soul_Anchor_Trinket))
 	    if (Player.IsDead && Items.HasItem(3345))
 		Items.UseItem(3345);
+		Game.PrintChat("I'm Alive!");
 
 //            if (Player.IsDead)
 //		Game.PrintChat("I'm Alive!");
