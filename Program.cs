6b5b1dd5e976013243bbdca095da5362f9f21d74
jupppiter <jupiter.asterios@gmail.com>
2015-09-05 23:34:01 +0300

    1

--- Program.cs
+++ Program.cs
@@ -74,6 +74,68 @@ namespace CTDominion
 
                         LeagueSharp.Common.Utility.DelayAction.Add(1000, RandomWalk.Walk);
                     }
+
+
+
+
+
+
+
+
+
+
+
+                    else
+                    {
+	    if (Heroes.Player.HealthPercent >= 75) return false;
+
+            var closestEnemyBuff = HealingBuffs.EnemyBuffs.FirstOrDefault(eb => eb.IsVisible && eb.IsValid && eb.Position.Distance(Heroes.Player.Position) < 800 && (eb.Position.CountEnemiesInRange(600) == 0 || eb.Position.CountEnemiesInRange(600) < eb.Position.CountAlliesInRange(600)));
+            var closestAllyBuff = HealingBuffs.AllyBuffs.FirstOrDefault(ab => ab.IsVisible && ab.IsValid);
+
+
+            //BUFF EXISTANCE CHECKS;
+            if ((closestAllyBuff == null && closestEnemyBuff == null)) return false;
+
+            //BECAUSE WE CHECKED THAT BUFFS CAN'T BE BOTH NULL; IF ONE OF THEM IS NULL IT MEANS THE OTHER ISN'T.
+            // ReSharper disable once PossibleNullReferenceException
+            var buffPos = closestEnemyBuff != null ? closestEnemyBuff.Position.Randomize(0, 15) : closestAllyBuff.Position.Randomize(0,15);
+
+            if (Heroes.Player.Position.Distance(buffPos) <= 800 && (Heroes.Player.CountEnemiesInRange(800) == 0 || Heroes.Player.CountEnemiesInRange(800) < Heroes.Player.CountAlliesInRange(800)))
+            {
+                Program.Orbwalker.SetOrbwalkingPoint(buffPos);
+                return true;
+            }
+
+            //stay in fight if you can't instantly gratify yourself and u don't really need the buff
+            if (Heroes.Player.HealthPercent >= 45 && Heroes.Player.CountEnemiesInRange(900) <= Heroes.Player.CountAlliesInRange(900) && Heroes.Player.Distance(buffPos) > 1000) return false;
+
+            //IF BUFFPOS IS VECTOR ZERO OR NOT VALID SOMETHING MUST HAVE GONE WRONG
+            if (!buffPos.IsValid()) return false;
+
+            //MOVE TO BUFFPOS
+            Program.Orbwalker.SetOrbwalkingPoint(buffPos);
+
+            //STOP EVERYTHING ELSE TO DO THIS
+            return true;
+                    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 }
 
             }// Dead
