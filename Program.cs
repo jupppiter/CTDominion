d87e89eb7ba9f434337641d1c78c0ae4c3afc973
jupppiter <jupiter.asterios@gmail.com>
2015-09-07 00:59:45 +0300

    1

--- Program.cs
+++ Program.cs
@@ -85,7 +85,58 @@ namespace CTDominion
 
 
 //jupppiter:
+    public static class HealingBuffs
+    {
+        private static List<GameObject> _healingBuffs;
+        private static int LastUpdate = 0;
 
+        public static List<GameObject> AllyBuffs
+        {
+            get { return _healingBuffs.FindAll(hb => hb.IsValid && LeagueSharp.Common.Geometry.Distance(hb.Position, HeadQuarters.AllyHQ.Position) < 5400).OrderBy(buff => buff.Position.Distance(Heroes.Player.Position)).ToList(); }
+        }
+
+        public static List<GameObject> EnemyBuffs
+        {
+            get { return _healingBuffs.FindAll(hb => hb.IsValid && LeagueSharp.Common.Geometry.Distance(hb.Position, HeadQuarters.AllyHQ.Position) > 5400); }
+        }
+
+        public static void Load()
+        {
+            _healingBuffs = ObjectManager.Get<GameObject>().Where(h=>h.Name.Contains("healingBuff")).ToList();
+            GameObject.OnCreate += OnCreate;
+            GameObject.OnDelete += OnDelete;
+            Game.OnUpdate += UpdateBuffs;
+        }
+
+        private static void UpdateBuffs(EventArgs args)
+        {
+            if (Environment.TickCount > LastUpdate + 1000)
+            {
+                foreach (var buff in _healingBuffs)
+                {
+                    if (Heroes.Player.ServerPosition.Distance(buff.Position) < 80) _healingBuffs.Remove(buff);
+                }
+                LastUpdate = Environment.TickCount;
+            }
+        }
+
+        private static void OnCreate(GameObject sender, EventArgs args)
+        {
+            if (sender.Name.Contains("healingBuff"))
+            {
+                _healingBuffs.Add(sender);
+            }
+        }
+
+        private static void OnDelete(GameObject sender, EventArgs args)
+        {
+            var iList = _healingBuffs.Where(buff => buff.NetworkId == sender.NetworkId);
+            foreach (var i in iList)
+            {
+                _healingBuffs.Remove(i);
+            }
+        }
+    }
 //jupppiter.
 
 
