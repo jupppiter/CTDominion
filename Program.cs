8dee0354ade9a30aa7bc07cced73c1c512c3eeb6
jupppiter <jupiter.asterios@gmail.com>
2015-09-07 00:36:04 +0300

    1

--- Program.cs
+++ Program.cs
@@ -178,8 +178,8 @@ namespace CTDominion
 //            if (Player.Distance(TEAM_POS) > 100 && Player.CountEnemiesInRange(1500) < 1 && Minions.EnemyMinions.Any(m => m.Distance(Player) < 100))
             if (Player.Distance(TEAM_POS) > 100 && !Minions.EnemyMinions.Any(m => m.Distance(Player) < 100))
 		// recall if no enemy near and minions
-		Game.PrintChat("minions...");
             {
+		Game.PrintChat("minions...");
            	Player.Spellbook.CastSpell(SpellSlot.Recall);
             }
             else
