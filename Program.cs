e5c7c339416d14525fd683ef3f7a09a2f75d748b
jupppiter <jupiter.asterios@gmail.com>
2015-09-06 15:28:42 +0300

    1

--- Program.cs
+++ Program.cs
@@ -55,7 +55,8 @@ namespace CTDominion
             {
 
 //                if (Player.Health < 300 && Player.CountEnemiesInRange(1200) > 3)
-                if (Player.HealthPercent <= 30 || Player.ManaPercent <= 20 && Player.CountEnemiesInRange(1200) > 3)
+//                if (Player.HealthPercent <= 30 || Player.ManaPercent <= 20 && Player.CountEnemiesInRange(1200) > 3)
+                if (Player.HealthPercent <= 30 && Player.CountEnemiesInRange(1200) > 3)
                 {
                     MoveBase();
 //                    Game.PrintChat("Home!");
