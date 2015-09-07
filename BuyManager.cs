796fc44c825afa4fbe6de16bad074d7890710d48
jupppiter <jupiter.asterios@gmail.com>
2015-09-07 17:19:40 +0300

    1

--- BuyManager.cs
+++ BuyManager.cs
@@ -30,7 +30,7 @@ namespace CTDominion
                 case "Ryze":
                     ItemsToBuy.Add(new CItem(3020, 1100, false)); // Sorcer Bots [1]
 //                    ItemsToBuy.Add(new CItem(3070, 720, false)); // Tear of the Goddess [2]
-                    ItemsToBuy.Add(new CItem(3003, 3000, false)); // Archangel's Staff [3]
+                    ItemsToBuy.Add(new CItem(3007, 3000, false)); // Archangel's Staff (Crystal Scar)
                     ItemsToBuy.Add(new CItem(3090, 3500, false)); // Wooglet's Witchcap [4]
 //                    ItemsToBuy.Add(new CItem(3135, 2500, false)); // Void Staff [5]
                     ItemsToBuy.Add(new CItem(3165, 2300, false)); // Morello [6]
